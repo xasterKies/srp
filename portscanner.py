322a7e5fcc213d9fc2b7d76d2ace48cfa12e4089
xaster-Kies <samueledward40@gmail.com>
2022-10-09 12:29:21 +0100

    open ports found

---
+++ portscanner.py
@@ -0,0 +1,28 @@
+import socket 
+
+
+
+def scan(target, ports):
+  for port in range(1, ports):
+    scan_port(target, port)
+
+def scan_port(ipaddress, port):
+  try:
+    sock = socket.socket() # Initiate the socket instance
+    sock.connect((ipaddress, port)) # Connect to our target
+    print("[+] Port Opened  " + str(port))
+    sock.close()
+  except:
+    pass
+
+targets = input("[*] Enter Targets To Scan(split them by ,): ")
+ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
+
+if ',' in targets:
+  print("[*] Scanning Multiple Targets")
+  for ip_addr in targets.split(','):
+    scan(ip_addr.strip(' '), ports)
+else:
+  scan(targets, ports)
+
+  
